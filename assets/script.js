d6ab975c525392bb823a855e77345a1b315796e2
Tom Overland <tomoverland03@gmail.com>
2020-07-23 07:06:52 -0500

    made changes to index.html

--- assets/script.js
+++ assets/script.js
@@ -6,9 +6,10 @@ let counter = document.getElementById("count");
 let start = document.getElementById("start");
 let question = document.getElementById("question");
 
+// This function will hide my start button on click, and reveal the buttons.
 function hideStart() {
-  console.log("Start");
-  console.log(start.style);
+  //console.log("Start");  This console.log will add "start" to the console to give me a quick idea if the button is working
+  //console.log(start.style);   This console.log tells me what the "style" of the start element is targeting
   if (start.classList.contains("show")) {
     start.classList.add("hide");
     start.classList.remove("show");
@@ -23,4 +24,5 @@ function hideStart() {
   }
 }
 
+//This on click event will trigger the hideStart function
 start.addEventListener("click", hideStart);
